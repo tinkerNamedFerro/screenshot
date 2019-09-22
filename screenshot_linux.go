aa9fad22e79e3734d0e3ab40cb98de92db95a3fb
ferro <f4rr0@protonmail.com>
2019-09-23 08:58:03 +1000

    changing screenshot_linux.go import function

--- screenshot_linux.go
+++ screenshot_linux.go
@@ -3,7 +3,7 @@ package screenshot
 import (
 	"image"
 
-	"github.com/kbinani/screenshot/internal/xwindow"
+	"github.com/tinkerNamedFerro/screenshot/internal/xwindow"
 )
 
 // Capture returns screen capture of specified desktop region.
